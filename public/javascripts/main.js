fefb5e68fd1abc8c8ebf5d32f187e4f51343d87b
Aditya <zeandcode@gmail.com>
2017-10-08 21:05:49 +0700

    Preparing leafs for separator

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-var S, Backsound;
+var S, Backsound, debug = true;
 var makeElFullScreen = function(selector) {
   var vpHeight = $(window).height();
   $(selector).height(vpHeight);
@@ -20,9 +20,44 @@ var init = function() {
   S = skrollr.init();
   makeElFullScreen('.scene');
   makeWeddingBackgroundFull();
+  displayScrollTop();
 }
 
+var displayScrollTop = function() {
+  if($('.debug-scroll-top').length == 0) {
+    $('body').append('<div class="debug-scroll-top"></div>');
+  }
 
+  $('.debug-scroll-top')
+    .css({
+      position: 'fixed',
+      right: 0,
+      top: 0,
+      background: 'rgba(0,0,0,.5)',
+      color: '#ffffff',
+      width: 50,
+      height: 50,
+      textAlign: 'center',
+      padding: 20,
+      boxSizing: 'border-box'
+    })
+    .html('<span class="number">'+$(document).scrollTop()+'</span>');
+}
+
+var scrolling = function(element, duration) {
+  $.scrollTo(element, duration);
+}
+
+var makeLeaflots = function(container) {
+  var leafSizes = ['medium', 'small'];
+  var leafTypes = ['yellow', 'orange', 'red-tail','yellow-tail'];
+  for (var i=-10; i<= 40; i++) {
+    var size = leafSizes[Math.floor(Math.random()*leafSizes.length)];
+    var type = leafTypes[Math.floor(Math.random()*leafTypes.length)];
+    var rotation = (Math.floor(Math.random() * 36) + 1) * 10;
+    container.append('<div class="sharp-leaf '+ size +' '+ type +' rotate-'+rotation+'" style="top: -90px; left: '+((i * 30) + 10)+'px;"></div>')
+  }
+}
 
 $(document).ready(function() {
   
@@ -40,6 +75,12 @@ $(document).ready(function() {
       sound.play();
       Backsound = sound;
     }
+
+    displayScrollTop();
   })
+
+  $('.top-boundaries').width($(document).outerWidth());
+
+  makeLeaflots($('#scene-two .top-boundaries'));
   
 });
