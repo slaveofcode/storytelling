e2d593f969c1abcbfc7db9ca007a4be93da7939b
Aditya <zeandcode@gmail.com>
2017-10-08 14:11:22 +0700

    First scene

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,4 @@
+var S;
 var makeElFullScreen = function(selector) {
   var vpHeight = $(window).height();
   $(selector).height(vpHeight);
@@ -7,7 +8,7 @@ var makeWeddingBackgroundFull = function () {
   var vpHeight = $('#scene-one').height()
       vpWidth = $('#scene-one').width();
 
-  var imgSrc = 'https://images.unsplash.com/photo-1470322450444-8ccc64987ccf?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w='+vpWidth+'&h='+vpHeight+'&fit=crop&s=0143c240e54dc4df3577a5df7fc6f9fc';
+  var imgSrc = 'https://images.unsplash.com/photo-1477414348463-c0eb7f1359b6?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w='+vpWidth+'&h='+vpHeight+'&fit=crop&s=4df8f538129b1a78f3c80c9834d67442';
   $('#wedding-background').css({
     background: 'url('+imgSrc+') no-repeat center center fixed',
     width: vpWidth,
@@ -15,59 +16,53 @@ var makeWeddingBackgroundFull = function () {
   });
 }
 
-var normalizeScene = function() {
-  var vpWidth = $(window).width(),
-      maxWidth = 1280;
-  $('#container').width(vpWidth > maxWidth ? maxWidth : vpWidth);
-}
-
 var init = function() {
+  S = skrollr.init();
   makeElFullScreen('.scene');
   makeWeddingBackgroundFull();
-  normalizeScene();
 }
 
-var sceneOne = function() {
-  var timeline = new TimelineMax();
+// var sceneOne = function() {
+//   var timeline = new TimelineMax();
   
-  var tweenLogo = new TweenMax.to('#logo', 1.2, {
-    marginTop: '10vh'
-  });
+//   var tweenLogo = new TweenMax.to('#logo', 1.2, {
+//     marginTop: '10vh'
+//   });
   
-  var tween2 = new TweenMax.to('#text-welcome', 1.2, {
-    marginLeft: '20vh'
-  });
+//   var tween2 = new TweenMax.to('#text-welcome', 1.2, {
+//     marginLeft: '20vh'
+//   });
   
-  var tween3 = new TweenMax.to('#text-subtitle', 1.2, {
-    marginRight: '10vh'
-  });
+//   var tween3 = new TweenMax.to('#text-subtitle', 1.2, {
+//     marginRight: '10vh'
+//   });
   
-  var tween4 = new TweenMax.to('#menu-bar', 1.2, {
-    marginTop: 0
-  });
+//   var tween4 = new TweenMax.to('#menu-bar', 1.2, {
+//     marginTop: 0
+//   });
   
-  timeline.add(tweenLogo);
-  timeline.add(tween2);
-  timeline.add(tween3);
-  timeline.add(tween4);
+//   timeline.add(tweenLogo);
+//   timeline.add(tween2);
+//   timeline.add(tween3);
+//   timeline.add(tween4);
 
-  var sceneOne = new ScrollMagic.Scene({
-    triggerElement: '#scene-one'
-  })
-  .setTween(timeline);
+//   var sceneOne = new ScrollMagic.Scene({
+//     triggerElement: '#scene-one'
+//   })
+//   .setTween(timeline);
 
-  return sceneOne;
-}
+//   return sceneOne;
+// }
 
 $(document).ready(function() {
   
   init();
 
-  var ctrl = new ScrollMagic.Controller({
-    container: '#canvas'
-  });
+//   var ctrl = new ScrollMagic.Controller({
+//     container: '#canvas'
+//   });
   
-  ctrl.addScene([
-    sceneOne()
-  ]);
+//   ctrl.addScene([
+//     sceneOne()
+//   ]);
 });
